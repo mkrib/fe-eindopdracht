93cb039ed98189ccba5c923c57bfde7c97e5477b
mkrib <mkribben@mac.com>
2024-04-15 18:49:55 +0200

    global setup login and reservation page

--- src/pages/loginRegister/LoginRegister.jsx
+++ src/pages/loginRegister/LoginRegister.jsx
@@ -1,8 +1,38 @@
+import './LoginRegister.css'
+
 const LoginRegister = () => {
     return (
-        <div>
-            <h1>Login register page</h1>
-        </div>
+        <>
+            <main className="outer-container">
+                <section className="inner-container">
+
+                    <h3>Inloggen</h3>
+                    <form className="form-login">
+                        <label htmlFor="email">Emailadres</label>
+                        <input type="email" id="email" name="email" required/>
+                        <label htmlFor="password">Wachtwoord</label>
+                        <input type="password" id="password" name="password" required/>
+                        <button className="btn-login" type="submit">Login</button>
+                    </form>
+
+                    <h3>Nog geen account?</h3>
+                    <form className="form-register">
+                        <label htmlFor="fullname">Voor- en achternaam</label>
+                        <input type="text" id="fullname" name="fullname" required/>
+                        <label htmlFor="phonenumber">Telefoonnummer</label>
+                        <input type="tel" id="phonenumber" name="phonenumber" required/>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" required/>
+                        <label htmlFor="password">Wachtwoord</label>
+                        <input type="password" id="password" name="password" required/>
+                        <label htmlFor="password">Herhaal wachtwoord</label>
+                        <input type="password" id="password" name="password" required/>
+                        <button className="btn-register" type="submit">Registreer</button>
+                    </form>
+                </section>
+
+            </main>
+        </>
     );
 };
 
--- src/pages/makeReservation/MakeReservation.jsx
+++ src/pages/makeReservation/MakeReservation.jsx
@@ -1,8 +1,24 @@
+import './MakeReservation.css';
 const MakeReservation = () => {
     return (
-        <div>
-            <h1>Make Reservation</h1>
-        </div>
+        <>
+            <main className="outer-container">
+                <section className="inner-container">
+
+                    <h3>Reserveren</h3>
+                    <form className="form-make-reservation">
+                        <label htmlFor="date">Datum</label>
+                        <input type="date" id="date" name="date" required/>
+                        <label htmlFor="time">Tijd</label>
+                        <input type="time" id="time" name="time" required/>
+                        <label htmlFor="special-request">Speciaal verzoek</label>
+                        <textarea id="special-request" name="special-request"/>
+                        <button className="btn-login" type="submit">Reserveer</button>
+                    </form>
+                </section>
+
+            </main>
+        </>
     );
 };
 
