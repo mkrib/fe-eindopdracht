07c51f5e6a264dc066063b461e1eafff8571d385
mkrib <mkribben@mac.com>
2024-04-22 12:45:27 +0200

    added jump links to the side nav in the profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -68,142 +68,73 @@ const Profile = () => {
         <>
             {/*Onderstaande code runnen wanneer een admin is ingelogd*/}
 
-            {/*<div className="profile-side-nav">*/}
-            {/*    <ul>*/}
-            {/*        <li><Link to="/">Reserveringen</Link></li>*/}
-            {/*        <li><Link to="/">Reviews</Link></li>*/}
-            {/*        <li><Link to="/">Blog toevoegen</Link></li>*/}
-            {/*        <li><Link to="/">Log uit</Link></li>*/}
-            {/*    </ul>*/}
-            {/*</div>*/}
-
-            {/*<section className="profile-content">*/}
-            {/*    <h2>Reserveringen</h2>*/}
-            {/*    <div className="display-profile-content">*/}
-            {/*        <ul className="ul-reservations">*/}
-            {/*            {reservations && reservations.map((reservation) => {*/}
-            {/*                return (*/}
-            {/*                    <li key={reservation.id}>*/}
-            {/*                        <p>{formatDateWithWeekday(reservation.date)} om {formatTimeWithoutSeconds(reservation.startTime)}</p>*/}
-            {/*                        <p>{reservation.amountOfGuests} personen</p>*/}
-            {/*                        <p>Speciale verzoeken: {reservation.specialRequest}</p>*/}
-            {/*                        <Button*/}
-            {/*                            lightOrDark="btn-dark"*/}
-            {/*                            type="button"*/}
-            {/*                            handleClick={() => cancelReservation(reservation.id)}*/}
-            {/*                        >*/}
-            {/*                            Annuleer*/}
-            {/*                        </Button>*/}
-            {/*                    </li>*/}
-            {/*                )*/}
-            {/*            })}*/}
-
-            {/*            {reservations.length === 0 && <p>Er zijn momenteel geen reserveringen.</p>}*/}
-
-            {/*            {error && <p>Oeps, er ging iets mis met het ophalen van de reserveringen...</p>}*/}
-            {/*        </ul>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-
-            {/*<section className="profile-content">*/}
-            {/*    <h2>Reviews</h2>*/}
-            {/*    <div className="display-profile-content">*/}
-
-            {/*        <ul className="ul-reviews">*/}
-            {/*            {reviews && reviews.map((review) => {*/}
-            {/*                return (*/}
-            {/*                    <li key={review.id}>*/}
-            {/*                        <p>{review.content}</p>*/}
-            {/*                        <p>{review.fullName}</p>*/}
-            {/*                        <p>{formatDateWithWeekday(review.issueDate)}</p>*/}
-            {/*                        <Button*/}
-            {/*                            lightOrDark="btn-dark"*/}
-            {/*                            type="button"*/}
-            {/*                            handleClick={() => deleteReview(review.id)}*/}
-            {/*                        >*/}
-            {/*                            Verwijder*/}
-            {/*                        </Button>*/}
-            {/*                    </li>*/}
-            {/*                )*/}
-            {/*            })}*/}
-
-            {/*            {reviews.length === 0 && <p>Er zijn momenteel geen reviews.</p>}*/}
-
-            {/*            {error && <p>Oeps, er ging iets mis met het ophalen van de reviews...</p>}*/}
-            {/*        </ul>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-
-            {/*<section className="profile-content">*/}
-            {/*    <h2>Blog toevoegen</h2>*/}
-            {/*    <div className="display-profile-content">*/}
-            {/*        <form className="form-add-blog">*/}
-            {/*            <label htmlFor="title">Titel</label>*/}
-            {/*            <input type="text" id="title" name="title" required/>*/}
-            {/*            <label htmlFor="content">Content</label>*/}
-            {/*            <textarea className="textarea-content" id="content" name="content" required></textarea>*/}
-            {/*            <label htmlFor="previewText">Tekst voorvertoning</label>*/}
-            {/*            <textarea className="textarea-previewtext" id="previewText" name="previewText"*/}
-            {/*                      required></textarea>*/}
-            {/*            <label htmlFor="image">Afbeelding</label>*/}
-            {/*            <input type="file" id="image" name="image" required/>*/}
-            {/*            <Button*/}
-            {/*                lightOrDark="btn-light"*/}
-            {/*                type="submit"*/}
-            {/*            >*/}
-            {/*                Voeg toe*/}
-            {/*            </Button>*/}
-            {/*        </form>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-
-            {/*    Onderstaande code runnen wanneer een gast is ingelogd */}
-
             <div className="profile-side-nav">
                 <ul>
-                    <li><Link to="/">Reserveringen</Link></li>
-                    <li><Link to="/">Review achterlaten</Link></li>
-                    <li><Link to="/">Persoonlijke gegevens</Link></li>
+                    <li><a href="#id-reservations">Reserveringen</a></li>
+                    <li><a href="#id-reviews">Reviews</a></li>
+                    <li><a href="#id-add-blog">Blog toevoegen</a></li>
                     <li><Link to="/">Log uit</Link></li>
                 </ul>
             </div>
 
-            <section className="profile-content">
+            <section className="profile-content" id="id-reservations">
                 <h2>Reserveringen</h2>
                 <div className="display-profile-content">
                     <ul className="ul-reservations">
-                        {/*{reservations && reservations.filter((reservation) => {*/}
-                        {/*    return (*/}
-                        {/*        <li key={reservation.id}>*/}
-                        {/*            <p>{formatDateWithWeekday(reservation.date)} om {formatTimeWithoutSeconds(reservation.startTime)}</p>*/}
-                        {/*            <p>{reservation.amountOfGuests} personen</p>*/}
-                        {/*            <p>Speciale verzoeken: {reservation.specialRequest}</p>*/}
-                        {/*            <Button*/}
-                        {/*                lightOrDark="btn-dark"*/}
-                        {/*                type="button"*/}
-                        {/*                handleClick={() => cancelReservation(reservation.id)}*/}
-                        {/*            >*/}
-                        {/*                Annuleer*/}
-                        {/*            </Button>*/}
-                        {/*        </li>*/}
-                        {/*    )*/}
-                        {/*})}*/}
+                        {reservations && reservations.map((reservation) => {
+                            return (
+                                <li key={reservation.id}>
+                                    <p>{formatDateWithWeekday(reservation.date)} om {formatTimeWithoutSeconds(reservation.startTime)}</p>
+                                    <p>{reservation.amountOfGuests} personen</p>
+                                    <p>Speciale verzoeken: {reservation.specialRequest}</p>
+                                    <Button
+                                        lightOrDark="btn-dark"
+                                        type="button"
+                                        handleClick={() => cancelReservation(reservation.id)}
+                                    >
+                                        Annuleer
+                                    </Button>
+                                </li>
+                            )
+                        })}
 
-                        {/*{reservations.length === 0 && <p>U heeft momenteel geen reserveringen.</p>}*/}
+                        {reservations.length === 0 && <p>Er zijn momenteel geen reserveringen.</p>}
 
-                        {/*{error && <p>Oeps, er ging iets mis met het ophalen van de reserveringen...</p>}*/}
+                        {error && <p>Oeps, er ging iets mis met het ophalen van de reserveringen...</p>}
                     </ul>
                 </div>
             </section>
 
-            <section className="profile-content">
-                <h2>Review achterlaten</h2>
+            <section className="profile-content" id="id-reviews">
+                <h2>Reviews</h2>
                 <div className="display-profile-content">
-                    <AddReviewForm/>
+
+                    <ul className="ul-reviews">
+                        {reviews && reviews.map((review) => {
+                            return (
+                                <li key={review.id}>
+                                    <p>{review.content}</p>
+                                    <p>{review.fullName}</p>
+                                    <p>{formatDateWithWeekday(review.issueDate)}</p>
+                                    <Button
+                                        lightOrDark="btn-dark"
+                                        type="button"
+                                        handleClick={() => deleteReview(review.id)}
+                                    >
+                                        Verwijder
+                                    </Button>
+                                </li>
+                            )
+                        })}
+
+                        {reviews.length === 0 && <p>Er zijn momenteel geen reviews.</p>}
+
+                        {error && <p>Oeps, er ging iets mis met het ophalen van de reviews...</p>}
+                    </ul>
                 </div>
             </section>
 
-            <section className="profile-content">
+            <section className="profile-content" id="id-add-blog">
                 <h2>Blog toevoegen</h2>
                 <div className="display-profile-content">
                     <form className="form-add-blog">
@@ -225,6 +156,75 @@ const Profile = () => {
                     </form>
                 </div>
             </section>
+
+            {/*    Onderstaande code runnen wanneer een gast is ingelogd */}
+
+            {/*<div className="profile-side-nav profile-side-nav-guest">*/}
+            {/*    <ul>*/}
+            {/*        <li><a href="#id-reservations">Reserveringen</a></li>*/}
+            {/*        <li><a href="#id-leave-review">Review achterlaten</a></li>*/}
+            {/*        <li><a href="#id-personal-details">Persoonlijke gegevens</a></li>*/}
+            {/*        <li><Link to="/">Log uit</Link></li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
+
+            {/*<section className="profile-content" id="id-reservations">*/}
+            {/*    <h2>Reserveringen</h2>*/}
+            {/*    <div className="display-profile-content">*/}
+            {/*        <ul className="ul-reservations">*/}
+            {/*            /!*{reservations && reservations.filter((reservation) => {*!/*/}
+            {/*            /!*    return (*!/*/}
+            {/*            /!*        <li key={reservation.id}>*!/*/}
+            {/*            /!*            <p>{formatDateWithWeekday(reservation.date)} om {formatTimeWithoutSeconds(reservation.startTime)}</p>*!/*/}
+            {/*            /!*            <p>{reservation.amountOfGuests} personen</p>*!/*/}
+            {/*            /!*            <p>Speciale verzoeken: {reservation.specialRequest}</p>*!/*/}
+            {/*            /!*            <Button*!/*/}
+            {/*            /!*                lightOrDark="btn-dark"*!/*/}
+            {/*            /!*                type="button"*!/*/}
+            {/*            /!*                handleClick={() => cancelReservation(reservation.id)}*!/*/}
+            {/*            /!*            >*!/*/}
+            {/*            /!*                Annuleer*!/*/}
+            {/*            /!*            </Button>*!/*/}
+            {/*            /!*        </li>*!/*/}
+            {/*            /!*    )*!/*/}
+            {/*            /!*})}*!/*/}
+
+            {/*            /!*{reservations.length === 0 && <p>U heeft momenteel geen reserveringen.</p>}*!/*/}
+
+            {/*            /!*{error && <p>Oeps, er ging iets mis met het ophalen van de reserveringen...</p>}*!/*/}
+            {/*        </ul>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+
+            {/*<section className="profile-content" id="id-leave-review">*/}
+            {/*    <h2>Review achterlaten</h2>*/}
+            {/*    <div className="display-profile-content display-profile-content-small">*/}
+            {/*        <AddReviewForm/>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+
+            {/*<section className="profile-content" id="id-personal-details">*/}
+            {/*    <h2>Persoonlijke gegevens</h2>*/}
+            {/*    <div className="display-profile-content">*/}
+            {/*        <form className="form-add-blog">*/}
+            {/*            <label htmlFor="title">Titel</label>*/}
+            {/*            <input type="text" id="title" name="title" required/>*/}
+            {/*            <label htmlFor="content">Content</label>*/}
+            {/*            <textarea className="textarea-blog-content" id="content" name="content" required></textarea>*/}
+            {/*            <label htmlFor="previewText">Tekst voorvertoning</label>*/}
+            {/*            <textarea className="textarea-blog-previewtext" id="previewText" name="previewText"*/}
+            {/*                      required></textarea>*/}
+            {/*            <label htmlFor="image">Afbeelding</label>*/}
+            {/*            <input type="file" id="image" name="image" required/>*/}
+            {/*            <Button*/}
+            {/*                lightOrDark="btn-light"*/}
+            {/*                type="submit"*/}
+            {/*            >*/}
+            {/*                Voeg toe*/}
+            {/*            </Button>*/}
+            {/*        </form>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
 
 
         </>
