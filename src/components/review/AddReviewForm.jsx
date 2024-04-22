361e4007a6dc9b2ad4bf4a88159481e1acb9c990
mkrib <mkribben@mac.com>
2024-04-22 11:58:31 +0200

    all reviews are being fetched in the admin profile - created a component to leave and post a review in the guest profile

---
+++ src/components/review/AddReviewForm.jsx
@@ -0,0 +1,88 @@
+import Button from "../buttons/Button.jsx";
+import './AddReviewForm.css';
+import {useState} from "react";
+import axios from "axios";
+
+const AddReviewForm = () => {
+    const [formValues, setFormValues] = useState({
+        nameReviewer: '',
+        content: '',
+
+        //     TODO username meegeven wanneer login is opgezet
+
+    });
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [error, setError] = useState(null);
+
+    function handleFormChange(e) {
+        const changedFieldName = e.target.name;
+        const newValue = e.target.value;
+
+        console.log(changedFieldName, newValue);
+
+        setFormValues({
+            ...formValues,
+            [changedFieldName]: newValue,
+        });
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        try {
+            const result = await axios.post('http://localhost:8080/reviewss', {
+                fullName: formValues.nameReviewer,
+                content: formValues.content,
+                issueDate: new Date(),
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log(result);
+            setIsSubmitted(true);
+        } catch (error) {
+            console.error(error.message);
+            setError(error.message);
+        }
+    }
+
+    return (
+        <>
+            {!isSubmitted &&
+                <form className="form-add-review" onSubmit={handleSubmit}>
+                    <label htmlFor="nameReviewer">Naam</label>
+                    <input type="text"
+                           id="nameReviewer"
+                           name="nameReviewer"
+                           value={formValues.nameReviewer}
+                           onChange={handleFormChange}
+                           required/>
+
+                    <label htmlFor="content">Content</label>
+                    <textarea className="textarea-review-content"
+                              id="content"
+                              name="content"
+                              value={formValues.content}
+                              onChange={handleFormChange}
+                              required></textarea>
+
+                    <Button
+                        lightOrDark="btn-dark"
+                        type="submit"
+                    >
+                        Verstuur
+                    </Button>
+
+                    {error && <p className="p-error">Er ging iets mis met het versturen van uw review. Probeer het opnieuw.</p>}
+                </form>
+            }
+
+                {isSubmitted &&
+                        <p>Bedankt voor het delen van uw ervaring!</p>}
+
+        </>
+    );
+};
+
+export default AddReviewForm;
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,9 +5,11 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import formatDateWithWeekday from "../../helpers/formatDateWithWeekday.jsx";
 import formatTimeWithoutSeconds from "../../helpers/formatTimeWithoutSeconds.jsx";
+import AddReviewForm from "../../components/review/AddReviewForm.jsx";
 
 const Profile = () => {
     const [reservations, setReservations] = useState([]);
+    const [reviews, setReviews] = useState([]);
     const [error, setError] = useState(null);
 
     async function fetchReservations() {
@@ -18,6 +20,17 @@ const Profile = () => {
                 return new Date(a.date) - new Date(b.date);
             });
             setReservations(sortedReservations);
+        } catch (error) {
+            console.error(error.message);
+            setError(error.message);
+        }
+    }
+
+    async function fetchReviews() {
+        try {
+            const result = await axios.get('http://localhost:8080/reviews');
+            console.log(result);
+            setReviews(result.data);
         } catch (error) {
             console.error(error.message);
             setError(error.message);
@@ -35,17 +48,122 @@ const Profile = () => {
         }
     }
 
+    async function deleteReview(id) {
+        try {
+            const result = await axios.delete(`http://localhost:8080/reviews/${id}`);
+            console.log(result);
+            await fetchReviews();
+        } catch (error) {
+            console.error(error.message);
+            setError(error.message);
+        }
+    }
+
     useEffect(() => {
         fetchReservations();
+        fetchReviews();
     }, []);
 
     return (
         <>
+            {/*Onderstaande code runnen wanneer een admin is ingelogd*/}
+
+            {/*<div className="profile-side-nav">*/}
+            {/*    <ul>*/}
+            {/*        <li><Link to="/">Reserveringen</Link></li>*/}
+            {/*        <li><Link to="/">Reviews</Link></li>*/}
+            {/*        <li><Link to="/">Blog toevoegen</Link></li>*/}
+            {/*        <li><Link to="/">Log uit</Link></li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
+
+            {/*<section className="profile-content">*/}
+            {/*    <h2>Reserveringen</h2>*/}
+            {/*    <div className="display-profile-content">*/}
+            {/*        <ul className="ul-reservations">*/}
+            {/*            {reservations && reservations.map((reservation) => {*/}
+            {/*                return (*/}
+            {/*                    <li key={reservation.id}>*/}
+            {/*                        <p>{formatDateWithWeekday(reservation.date)} om {formatTimeWithoutSeconds(reservation.startTime)}</p>*/}
+            {/*                        <p>{reservation.amountOfGuests} personen</p>*/}
+            {/*                        <p>Speciale verzoeken: {reservation.specialRequest}</p>*/}
+            {/*                        <Button*/}
+            {/*                            lightOrDark="btn-dark"*/}
+            {/*                            type="button"*/}
+            {/*                            handleClick={() => cancelReservation(reservation.id)}*/}
+            {/*                        >*/}
+            {/*                            Annuleer*/}
+            {/*                        </Button>*/}
+            {/*                    </li>*/}
+            {/*                )*/}
+            {/*            })}*/}
+
+            {/*            {reservations.length === 0 && <p>Er zijn momenteel geen reserveringen.</p>}*/}
+
+            {/*            {error && <p>Oeps, er ging iets mis met het ophalen van de reserveringen...</p>}*/}
+            {/*        </ul>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+
+            {/*<section className="profile-content">*/}
+            {/*    <h2>Reviews</h2>*/}
+            {/*    <div className="display-profile-content">*/}
+
+            {/*        <ul className="ul-reviews">*/}
+            {/*            {reviews && reviews.map((review) => {*/}
+            {/*                return (*/}
+            {/*                    <li key={review.id}>*/}
+            {/*                        <p>{review.content}</p>*/}
+            {/*                        <p>{review.fullName}</p>*/}
+            {/*                        <p>{formatDateWithWeekday(review.issueDate)}</p>*/}
+            {/*                        <Button*/}
+            {/*                            lightOrDark="btn-dark"*/}
+            {/*                            type="button"*/}
+            {/*                            handleClick={() => deleteReview(review.id)}*/}
+            {/*                        >*/}
+            {/*                            Verwijder*/}
+            {/*                        </Button>*/}
+            {/*                    </li>*/}
+            {/*                )*/}
+            {/*            })}*/}
+
+            {/*            {reviews.length === 0 && <p>Er zijn momenteel geen reviews.</p>}*/}
+
+            {/*            {error && <p>Oeps, er ging iets mis met het ophalen van de reviews...</p>}*/}
+            {/*        </ul>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+
+            {/*<section className="profile-content">*/}
+            {/*    <h2>Blog toevoegen</h2>*/}
+            {/*    <div className="display-profile-content">*/}
+            {/*        <form className="form-add-blog">*/}
+            {/*            <label htmlFor="title">Titel</label>*/}
+            {/*            <input type="text" id="title" name="title" required/>*/}
+            {/*            <label htmlFor="content">Content</label>*/}
+            {/*            <textarea className="textarea-content" id="content" name="content" required></textarea>*/}
+            {/*            <label htmlFor="previewText">Tekst voorvertoning</label>*/}
+            {/*            <textarea className="textarea-previewtext" id="previewText" name="previewText"*/}
+            {/*                      required></textarea>*/}
+            {/*            <label htmlFor="image">Afbeelding</label>*/}
+            {/*            <input type="file" id="image" name="image" required/>*/}
+            {/*            <Button*/}
+            {/*                lightOrDark="btn-light"*/}
+            {/*                type="submit"*/}
+            {/*            >*/}
+            {/*                Voeg toe*/}
+            {/*            </Button>*/}
+            {/*        </form>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+
+            {/*    Onderstaande code runnen wanneer een gast is ingelogd */}
+
             <div className="profile-side-nav">
                 <ul>
                     <li><Link to="/">Reserveringen</Link></li>
-                    <li><Link to="/">Reviews</Link></li>
-                    <li><Link to="/">Blog toevoegen</Link></li>
+                    <li><Link to="/">Review achterlaten</Link></li>
+                    <li><Link to="/">Persoonlijke gegevens</Link></li>
                     <li><Link to="/">Log uit</Link></li>
                 </ul>
             </div>
@@ -54,40 +172,34 @@ const Profile = () => {
                 <h2>Reserveringen</h2>
                 <div className="display-profile-content">
                     <ul className="ul-reservations">
-                        { reservations && reservations.map((reservation) => {
-                            return (
-                                <li key={reservation.id}>
-                                    <p>{formatDateWithWeekday(reservation.date)} om {formatTimeWithoutSeconds(reservation.startTime)}</p>
-                                    <p>{reservation.amountOfGuests} personen</p>
-                                    <p>Speciale verzoeken: {reservation.specialRequest}</p>
-                                    <Button
-                                    lightOrDark="btn-dark"
-                                    type="button"
-                                    handleClick={() => cancelReservation(reservation.id)}
-                                    >
-                                        Annuleer
-                                    </Button>
-                                </li>
-                            )
-                        })}
+                        {/*{reservations && reservations.filter((reservation) => {*/}
+                        {/*    return (*/}
+                        {/*        <li key={reservation.id}>*/}
+                        {/*            <p>{formatDateWithWeekday(reservation.date)} om {formatTimeWithoutSeconds(reservation.startTime)}</p>*/}
+                        {/*            <p>{reservation.amountOfGuests} personen</p>*/}
+                        {/*            <p>Speciale verzoeken: {reservation.specialRequest}</p>*/}
+                        {/*            <Button*/}
+                        {/*                lightOrDark="btn-dark"*/}
+                        {/*                type="button"*/}
+                        {/*                handleClick={() => cancelReservation(reservation.id)}*/}
+                        {/*            >*/}
+                        {/*                Annuleer*/}
+                        {/*            </Button>*/}
+                        {/*        </li>*/}
+                        {/*    )*/}
+                        {/*})}*/}
 
-                        {reservations.length === 0 && <p>Er zijn momenteel geen reserveringen.</p>}
+                        {/*{reservations.length === 0 && <p>U heeft momenteel geen reserveringen.</p>}*/}
 
-                        {error && <p>Oeps, er ging iets mis met het ophalen van de reserveringen...</p>}
+                        {/*{error && <p>Oeps, er ging iets mis met het ophalen van de reserveringen...</p>}*/}
                     </ul>
                 </div>
             </section>
 
             <section className="profile-content">
-                <h2>Reviews</h2>
+                <h2>Review achterlaten</h2>
                 <div className="display-profile-content">
-                    <ul>
-                        <li>
-                            <p>Bericht review</p>
-                            <p>Naam reviewer</p>
-                            <p>Datum van review</p>
-                        </li>
-                    </ul>
+                    <AddReviewForm/>
                 </div>
             </section>
 
@@ -95,23 +207,26 @@ const Profile = () => {
                 <h2>Blog toevoegen</h2>
                 <div className="display-profile-content">
                     <form className="form-add-blog">
-                    <label htmlFor="title">Titel</label>
+                        <label htmlFor="title">Titel</label>
                         <input type="text" id="title" name="title" required/>
                         <label htmlFor="content">Content</label>
-                        <textarea className="textarea-content" id="content" name="content" required></textarea>
+                        <textarea className="textarea-blog-content" id="content" name="content" required></textarea>
                         <label htmlFor="previewText">Tekst voorvertoning</label>
-                        <textarea className="textarea-previewtext" id="previewText" name="previewText" required></textarea>
+                        <textarea className="textarea-blog-previewtext" id="previewText" name="previewText"
+                                  required></textarea>
                         <label htmlFor="image">Afbeelding</label>
                         <input type="file" id="image" name="image" required/>
                         <Button
-                        lightOrDark="btn-light"
-                        type="submit"
+                            lightOrDark="btn-light"
+                            type="submit"
                         >
                             Voeg toe
                         </Button>
                     </form>
                 </div>
             </section>
+
+
         </>
     );
 };
